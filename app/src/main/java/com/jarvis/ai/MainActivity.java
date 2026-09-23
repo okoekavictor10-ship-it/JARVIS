3f35d8f052152a2988399814ad244088ddbcdbc0
okoekavictor10-ship-it <okoekavictor10@gmail.com>
2026-09-23 12:32:41 +0100

    Update MainActivity.java

--- app/src/main/java/com/jarvis/ai/MainActivity.java
+++ app/src/main/java/com/jarvis/ai/MainActivity.java
@@ -1,1 +1,24 @@
+package com.jarvis.ai;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView textView = new TextView(this);
+        textView.setText("JARVIS");
+        textView.setTextSize(32);
+        textView.setTextColor(Color.WHITE);
+        textView.setGravity(Gravity.CENTER);
+        textView.setBackgroundColor(Color.BLACK);
+
+        setContentView(textView);
+    }
+  }
